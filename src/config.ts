f7cca8236791ba4b2ef123fa42c3511cb456ea3b
KshitizPratap <87176945+KshitizPratap@users.noreply.github.com>
2025-12-06 18:57:44 +0530

    Update Redirect URI

--- src/config.ts
+++ src/config.ts
@@ -34,8 +34,7 @@ export const config = {
   META_CLIENT_SECRET: process.env.META_CLIENT_SECRET || '',
   // This is the redirect URI where Facebook sends the authorization code back to our backend
   // Must match exactly what's configured in Facebook App settings
-  // META_REDIRECT_URI: process.env.META_REDIRECT_URI || '',
-    META_REDIRECT_URI: "https://dev-revenue-pro-backend-1057029383450.asia-southeast1.run.app/api/v1/generate-meta-access-token/",
+  META_REDIRECT_URI: process.env.META_REDIRECT_URI || '',
   META_API_VERSION: process.env.META_API_VERSION || '',
  
 };
